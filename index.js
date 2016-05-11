7fe9f05665f15d85c51273ef64a65ae297fa3fbb
RobinWeipengChen <dayuqi@yeah.net>
2016-05-11 16:20:11 +0800

    换成支持aes加密的wechat npm

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-var  wechat = require('node-wechat')('mytoken');
-
+var wechat = require('wechat');
 
 app.set('port', (process.env.PORT || 80));
 
@@ -15,51 +14,48 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.all('/wechat', function (req, res, next) {
-  wechat.checkSignature(req, res);
+var config = {
+ token: 'mytoken',
+ appid: 'wx84997a56559d622d',
+ encodingAESKey: 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG'
+};
 
-  next(); // pass control to the next handler
-});
-
-app.get('/wechat', function(request, response) 
-{
-//检验 token
-  console.log('Node app is running on wechat');
-  wechat.checkSignature(request, response);
-});
-
-app.post('/wechat', function(request, response) {
-//检验 token
-  wechat.checkSignature(request, response);
-
-    wechat.handler(request, response);
-
- //监听文本信息
-  wechat.text(function (data) {
-
-    //console.log(data.ToUserName);
-    //console.log(data.FromUserName);
-    //console.log(data.CreateTime);
-    //console.log(data.MsgType);
-    //...
-
-    var msg = {
-      FromUserName : data.ToUserName,
-      ToUserName : data.FromUserName,
-      //MsgType : "music",
-      Title : "宋冬野",
-      Description : "宋冬野——摩登天空7",
-      MusicUrl : "http://zhangmenshiting.baidu.com/data2/music/71272862/44897031226800128.mp3?xcode=8c25fcb0e8157c1d4ee014e7c541cba8c3b34145ef4199ad",
-      //HQMusicUrl : "http://zhangmenshiting.baidu.com/data2/music/71272862/44897031226800128.mp3?xcode=8c25fcb0e8157c1d4ee014e7c541cba8c3b34145ef4199ad",
-      //FuncFlag : 0
-    }
-
-    //回复信息
-    wechat.send(msg);
-  });
-
-
-});
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
