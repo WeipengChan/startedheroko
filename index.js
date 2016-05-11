e04658ed20d9947f40eaba31cc181b02fa5bd64f
RobinWeipengChen <dayuqi@yeah.net>
2016-05-11 14:41:01 +0800

    add wechat

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var   wechat = require('node-wechat')('mytoken');
-
 
 app.set('port', (process.env.PORT || 80));
 
@@ -11,24 +9,12 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-
-  res.render('pages/index');
-});
-
-app.get('/wechat', function(req, res) {
-
-   //检验 token
-  wechat.checkSignature(req, res);
-  //预处理
-  wechat.handler(req, res);
-
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-
-
 });
 
 
